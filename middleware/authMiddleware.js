c59067f4fb6063cdc533f5b64f0625209a990935
user-blank1 <linaszilinskasas@gmail.com>
2025-09-17 18:34:30 +0300

    popular card endpoints

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,9 +2,10 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 const requireAuth = (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];
+    const token = req.headers.Authorization?.split(" ")[1] || req.headers.authorization?.split(" ")[1];
+
     if (!token) {
-        return res.status(401).json({ error: "Unauthorized" });
+        return res.status(401).json({ error: "Token was not provided" });
     }
     jwt.verify(token, process.env.SECRET, (err, decoded) => {
         if (err) {
